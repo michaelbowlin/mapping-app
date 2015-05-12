c5ec6909fca16fb04143c4afa05ba265b6484c37
BrianChildress <brian@redchilidigital.com>
2015-05-12 06:40:32 -0600

    Updating users to include linked properties

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,25 @@
 var mongoose = require('mongoose'),
+    properties = require('property.js'),
     encrypt = require('../utilities/encryption');
 
+var addressSchema = mongoose.Schema({
+  street: {type: String},
+  city: {type: String},
+  state: {type: String},
+  zipCode: {type: Number}
+});
+
+var billingSchema = mongoose.Schema({
+  accountType: {type: String},
+  accountContact: {type: String}
+});
+
 var userSchema = mongoose.Schema({
+  dateCreated: {type: Date, required:'{PATH} is required!', default: Date.now()},
+  dateUpdated: {type: Date, required:'{PATH} is required!', default: Date.now()},
   firstName: {type:String, required:'{PATH} is required!'},
   lastName: {type:String, required:'{PATH} is required!'},
+  accountKey: {type:String},
   username: {
     type: String,
     required: '{PATH} is required!',
@@ -11,8 +27,12 @@ var userSchema = mongoose.Schema({
   },
   salt: {type:String, required:'{PATH} is required!'},
   hashed_pwd: {type:String, required:'{PATH} is required!'},
-  roles: [String]
+  roles: [String],
+  properties: [{type: mongoose.Schema.Types.ObjectId, ref: 'Property'}],
+  address: [addressSchema],
+  billing: [billingSchema]
 });
+
 userSchema.methods = {
   authenticate: function(passwordToMatch) {
     return encrypt.hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
