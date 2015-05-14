ca201818f39d460f52464284236f4e7d08b4a57e
Michael Bowlin <info@michaebowlin.com>
2015-05-14 05:51:34 -0600

    Added in second directive to control the fullpage styling. Not an ideal implementation but it's currently working.

--- public/app/widgets/styling-no-fullscreen.directive.js
+++ public/app/widgets/styling-no-fullscreen.directive.js
@@ -3,19 +3,17 @@
 
     angular
         .module('app')
-        .directive('mpDeFullscreen', mpDeFullscreen);
+        .directive('mpDeNoFullscreen', mpDeNoFullscreen);
 
-    function mpDeFullscreen(){
+    function mpDeNoFullscreen(){
             return {
                 restrict: 'E',
                 replace: true,
                 link: function(scope, element, attrs){
                     var wrap = angular.element(document.getElementById('wrap'));
                     var menu = angular.element(document.getElementById('main-menu'));
-                    wrap.removeClass('wrap-normal');
-                    wrap.addClass('wrap-full');
-                    menu.removeClass('show')
-                    menu.addClass('hide');
+                    wrap.removeClass('wrap-full').addClass('wrap-normal');
+                    menu.removeClass('hide').addClass('show');
                 }
             }
         };
