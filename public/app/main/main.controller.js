bc44566117804e2f353eba5f4a1ecc0dfd33cfb8
BrianChildress <brian@redchilidigital.com>
2015-05-28 06:36:24 -0600

    Fixing cached properties

--- public/app/main/main.controller.js
+++ public/app/main/main.controller.js
@@ -5,11 +5,11 @@
     .module('app')
     .controller('main.controller', mainController);
 
-  function mainController(mvCachedCourses, mvCachedProperties) {
+  function mainController(mvCachedCourses, cachedPropertiesService) {
     var vm = this;
 
       vm.courses = mvCachedCourses.query();
-      vm.properties = mvCachedProperties.query();
+      vm.properties = cachedPropertiesService.query();
   };
 
 
