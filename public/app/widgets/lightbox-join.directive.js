4a88f4ddff75ab4e8c16e4ecb75183c811f3aa0f
Michael Bowlin <info@michaebowlin.com>
2015-06-09 06:17:57 -0600

    Added in right lightbox.

---
+++ public/app/widgets/lightbox-join.directive.js
@@ -0,0 +1,23 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('mpCoLbJoin', mpCoLbJoin);
+
+    function mpCoLbJoin(){
+        return {
+            restrict: 'E',
+            templateUrl: '/app/widgets/lightbox-join.html',
+            link: function(scope, element, attrs){
+
+            },
+            controller: function($scope){
+
+
+            }
+        }
+    };
+
+
+})();
