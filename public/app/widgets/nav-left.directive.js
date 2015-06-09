b91c6cf7146bb41a2e521896f3729bae9cc4c5f5
Michael Bowlin <info@michaebowlin.com>
2015-06-08 21:29:37 -0600

    Fixed nav and header.

--- public/app/widgets/nav-left.directive.js
+++ public/app/widgets/nav-left.directive.js
@@ -8,9 +8,32 @@
     function mpCoLeftnav(){
         return {
             restrict: 'E',
-            replace: true,
             templateUrl: '/app/widgets/nav-left.html',
-            link: function(scope, element, attrs){
+            link: function(scope, element){
+                //var config = scope.mpCoLeftnav;
+                //if(!config || !config.selector){
+                //    return;
+                //}
+                //element.bind('click', function(){
+                //    alert("sadf" + element);
+                //    var leftNavEl = angular.element(document.querySelector(congig.selector));
+                //    alert(leftNavEl);
+                //});
+
+                var navToggle = angular.element(document.getElementById("nav-toggle"));
+                var navWrap = angular.element(document.getElementById("nav-wrap"));
+
+                navToggle.bind('click', function(){
+                   if(navWrap.hasClass('expand')){
+                       navWrap.removeClass('expand');
+                   } else if (!navWrap.hasClass('expand')) {
+                       navWrap.addClass('expand');
+                   }
+                });
+
+
+
+
 
             },
             controller: function(){
